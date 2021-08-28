ad81f746b936e1512b517120727350e82aa2b264
Zahid046 <zmemon046@gmail.com>
2021-08-28 13:13:30 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/food_media/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.food_media
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:food_media/views/Welcome.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Welcome(),
+    );
+  }
+}
